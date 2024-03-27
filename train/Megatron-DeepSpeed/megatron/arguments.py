a93d0088fe65ed0970e93c6f20bf7c816ce2af8c
Tatsu <tatsunari1020@gmail.com>
2024-03-27 23:05:24 +0100

    debugged mistral config

--- train/Megatron-DeepSpeed/megatron/arguments.py
+++ train/Megatron-DeepSpeed/megatron/arguments.py
@@ -618,9 +618,9 @@ def _add_network_size_args(parser):
                        'attention. This is set to '
                        '   args.hidden_size // args.num_attention_heads '
                        'if not provided.')
-    group.add_argument('--window-size', type=int, default=None,
+    group.add_argument('--window-size', type=int, default=[-1, -1],
                        help='Window size for local attention.')
-    group.add_argument('--paged-kv-block-size', type=int, default=None,
+    group.add_argument('--paged-kv-block-size', type=int, default=0,
                        help='Block size for paged QK cache.')
     group.add_argument('--max-position-embeddings', type=int, default=None,
                        help='Maximum number of position embeddings to use. '
--- train/Megatron-DeepSpeed/megatron/model/transformer.py
+++ train/Megatron-DeepSpeed/megatron/model/transformer.py
@@ -370,7 +370,7 @@ class FlashSelfAttention(torch.nn.Module):
                            (default: 0.0)
     """
     def __init__(self, causal=False, softmax_scale=None, attention_dropout=0.0,
-                 window_size=(-1, 1), paged_kv_block_size=None,
+                 window_size=(-1, 1), paged_kv_block_size=0,
                  device=None, dtype=None):
         super().__init__()
         assert flash_attn_unpadded_func is not None or flash_attn_varlen_func is not None or flash_attn_builder is not None, \
@@ -427,7 +427,7 @@ class FlashSelfAttention(torch.nn.Module):
                                         device=q.device) if get_accelerator().device_name() == 'cuda' else None
             dropout_p = 0
 
-        if self.paged_kv_block_size:
+        if self.paged_kv_block_size != 0:
             num_blocks = math.ceil(seqlen_k / self.paged_kv_block_size) * batch_size
             block_table = rearrange(
                 torch.randperm(num_blocks, dtype=torch.int32, device=get_accelerator().device_name()),
@@ -518,7 +518,7 @@ class ParallelAttention(MegatronModule):
         self.num_key_value_heads = config.num_key_value_heads
         self.use_gqa = (self.num_attention_heads != self.num_key_value_heads)
         # Sliding Window Attention
-        self.window_size = (config.window_size, config.window_size)
+        self.window_size = (config.window_size[0], config.window_size[1])
 
         self.use_flash_attn = (args.use_flash_attn_v1 or args.use_flash_attn_triton or args.use_flash_attn_v2) \
                               and attention_type == AttnType.self_attn \
