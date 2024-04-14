91d6cf85704b1a3449d7d08d7526296f8eccee6d
Tatsu <tatsunari1020@gmail.com>
2024-04-14 18:20:10 +0200

    added paged attn training script

---
+++ train/scripts/step2_pretrain_model/jobs/pretrain_mistral_paged_attn.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Command line options go here
+#SBATCH --partition=g2
+#SBATCH --time=06:00:00
+#SBATCH --nodes=1
+#SBATCH --job-name=pretrain_mistral
+#SBATCH --output=./jobs/outs/pretrain_mistral_paged_attn.out
+#SBATCH --gpus-per-node=4
+
+# Command(s) goes here
+bash ~/ucllm_nedo_prod_reproduce/train/scripts/step2_pretrain_model/gcp_node-1_gpu/dataset-arxiv_tokenizer-sentencepiece_model-gpt_0.125B/pretrain_mistral_paged_attn.sh --input_tokenizer_file ~/ucllm_nedo_prod_reproduce/train/output/step1_train_tokenizer/botchan/botchan.model --output_model_dir ~/../../persistentshare/storage/team_haijima/tatsu/model_checkpoint/ --save_interval 700
