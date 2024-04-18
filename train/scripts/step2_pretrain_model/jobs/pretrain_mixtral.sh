27df4d5f3ad78507dd4abced69d52882271eaceb
Tatsu <tatsunari1020@gmail.com>
2024-04-18 09:45:06 +0200

    modified batch size for ds moe

--- train/scripts/step2_pretrain_model/jobs/pretrain_mixtral.sh
+++ train/scripts/step2_pretrain_model/jobs/pretrain_mixtral.sh
@@ -5,7 +5,7 @@
 #SBATCH --time=06:00:00
 #SBATCH --nodes=1
 #SBATCH --job-name=pretrain_mixtral
-#SBATCH --output=./jobs/outs/pretrain_mixtral_final.out
+#SBATCH --output=./jobs/outs/pretrain_mixtral_ds_moe_final.out
 #SBATCH --gpus-per-node=4
 
 # Command(s) goes here
